2867d4f4f6084a95faa7fa3ce0216c1cb3780117
SteveK <stevendeankennedy@gmail.com>
2016-09-07 14:54:09 -0400

    First commit.  Empty Hello World project.

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿// DESCRIPTION
+// (c) 2016 Steven Dean Kennedy
+
+using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
