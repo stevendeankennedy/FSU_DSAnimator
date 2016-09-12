0e17e4481a4538721ef69090515ea94a11d6f266
saulpena <sapenagamero0@frostburg.edu>
2016-09-12 17:38:21 -0400

    commit 1

---
+++ Assets/Scripts/DataStructures/Stack/GoCrazy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoCrazy : MonoBehaviour {
+
+    public Text crazyText;
+    int[] testArray;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GoingCrazy()
+    {
+        crazyText.text = "It went crazy";
+    }
+}
---
+++ Assets/Scripts/Menu/ButtonMenuScreen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+// This script is for the buttons in the menu screen. Each button will take the user to a different scene about a data structure.
+
+
+public class ButtonMenuScreen : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void goToStack()
+    {
+        Application.LoadLevel("Stack");
+    }
+
+    public void goToQueue()
+    {
+        Application.LoadLevel("Queue");
+    }
+
+    public void goToBubbleSort()
+    {
+        Application.LoadLevel("BubbleSort");
+    }
+
+    public void goToLinearSearch()
+    {
+        Application.LoadLevel("Stack");
+    }
+}
